9096e68d52dc72aeca485ff795c983911825e4f3
Çağdaş Güleç <102041399+Cagdas-Gulec@users.noreply.github.com>
2024-05-28 22:16:31 +0300

    UML's folder created and Relationship Diagram moved to the UML's folder.

--- RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b9efeabd39cf20422c28e2e2fee3604c8c2555f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+719ab1da56e61a54994825256bdb237133a80f14")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
